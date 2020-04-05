dae15d15ca558d5b805999079fd2b05e0a540c8b
Sunny Lee <leesunny507@gmail.com>
2020-04-05 01:38:24 -0400

    added logging tool and API handling

--- src/app.js
+++ src/app.js
@@ -4,30 +4,28 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const { NODE_ENV } = require('./config')
+const validateToken = require('./validateToken')
+const errorHandler = require('./error-handler')
+
+
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(validateToken)
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+app.use(errorHandler)
 
-module.exports = app;
+module.exports = app
