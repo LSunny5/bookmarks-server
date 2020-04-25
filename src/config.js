0959c4d8ac69073d75bba6f92c5cf51bfbf496dc
Sunny Lee <leesunny507@gmail.com>
2020-04-25 03:01:08 -0400

    changes for tests

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+    NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
     TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/bookmarks-test',
   }
